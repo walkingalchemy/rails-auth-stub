7f5ca500c8423f77db996ace7711ea7177d68284
walkingalchemy <walkingalchemy@gmail.com>
2017-12-25 23:23:04 -0500

    Stub out UsersController actions and private methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+  before_action set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to user_path(@user)
+    else
+      flash[:error] = @user.errors.full_messages
+      redirect_to new_user_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+  end
 end
