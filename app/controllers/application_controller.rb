0b3f03231d3fe8e0a8a27c1b4e0666604be0c73c
walkingalchemy <walkingalchemy@gmail.com>
2017-12-26 14:50:18 -0500

    Define #authorized and other helper methods in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authorized
+
+  helper_method: :logged_in?
+
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout
+    session[:user_id] = nil
+  end
+
+  def authorized
+    if !logged_in
+      redirect_to login_path
+    end
+  end
+
 end
