541728aa69db1af1e09e9a1ed9c3609ae641c405
walkingalchemy <walkingalchemy@gmail.com>
2017-12-26 15:25:07 -0500

    Build first routes for login flow and basic login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :users
+  get "/login", to: 'sessions#new', as: 'login'
+
+  post '/sessions', to: 'sessions#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
